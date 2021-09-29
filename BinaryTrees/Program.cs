d2c2401a6f50999b6d3aa89b1750034de102255d
Juan.Torres <ginsjuan@gmail.com>
2021-09-28 20:41:10 -0500

    test changed

--- BinaryTrees/Program.cs
+++ BinaryTrees/Program.cs
@@ -4,7 +4,7 @@ using BinaryTrees.Traverse;
 
 
 Preorder preorder = new Preorder();
-var result = preorder.PreorderTraversal(TreeNode.CreateTree("[1,null,2]"));
+var result = preorder.PreorderTraversal(TreeNode.CreateTree("[3,1,2]"));
 
 result.ToList().ForEach(n => Console.Write($"{n},"));
 Console.ReadKey();
